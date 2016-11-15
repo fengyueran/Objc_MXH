780a09fc199f60b032c165a23fd66592123ce930
xinghun meng <xinghunm@curacloudcorp.com>
2016-11-15 11:39:16 +0800

    init the issue-4-full-core-data-application repository

---
+++ issue-4-full-core-data-application/issue-4-full-core-data-application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  issue-4-full-core-data-application
+//
+//  Created by intern08 on 11/15/16.
+//  Copyright © 2016 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ issue-4-full-core-data-application/issue-4-full-core-data-application/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  issue-4-full-core-data-application
+//
+//  Created by intern08 on 11/15/16.
+//  Copyright © 2016 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
